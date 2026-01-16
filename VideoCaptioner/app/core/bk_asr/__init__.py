f73358475707d01dc643f6100b140c054ca8a2f7
HAIGC <18620144784@139.com>
2026-01-16 16:15:07 +0800

    Add files via upload

--- VideoCaptioner/app/core/bk_asr/__init__.py
+++ VideoCaptioner/app/core/bk_asr/__init__.py
@@ -1,1 +1,11 @@
+from .bcut import BcutASR
+from .faster_whisper import FasterWhisperASR
+from .jianying import JianYingASR
+from .transcribe import transcribe
 
+__all__ = [
+    "BcutASR",
+    "JianYingASR",
+    "FasterWhisperASR",
+    "transcribe",
+]
